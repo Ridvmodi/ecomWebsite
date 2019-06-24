edc04272fea0533160458d1bd9deceb37dcfe6df
Ridhav Modi <ridhavgarg01@gmail.com>
2019-06-24 15:24:38 +0530

    config change

---
+++ src/pages/contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Heading from '../components/Reuseable/Heading'
+export default function contact() {
+    return (
+        <div>
+            <Heading title="Conatct" />
+        </div>
+    )
+}
